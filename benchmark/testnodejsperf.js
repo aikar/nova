c66bac3870501adcf73821225a2d77c81092b3fd
Daniel <aikar@TYRIAL.(none)>
2011-02-28 23:43:43 -0500

    updated readme syntax errors/suggestions added komodo project

--- benchmark/testnodejsperf.js
+++ benchmark/testnodejsperf.js
@@ -1,14 +1,14 @@
 (function() {
   var sharedVariables = {
-   header: "Header",
-   header2: "Header2",
-   header3: "Header3",
-   header4: "Header4",
-   header5: "Header5",
-   header6: "Header6",
-   list: Array(50).join('test ').split(' ')
+    header: "Header",
+    header2: "Header2",
+    header3: "Header3",
+    header4: "Header4",
+    header5: "Header5",
+    header6: "Header6",
+    list: Array(50).join('test ').split(' ')
   };
-  
+
   var nova = require('../lib/nova/nova.js');
   var count = 5000;
   var sys = require('util');
@@ -19,8 +19,8 @@
   for (var i = 0; i < count; i++) {
     template.render(sharedVariables);
   }
-  
+
     var diff = new Date().getTime() - start;
     sys.puts('rendered ' + count + ' times in ' + diff + 'ms!', 'avg: ' + (diff / count)+'ms!');
-  
+
 })();
